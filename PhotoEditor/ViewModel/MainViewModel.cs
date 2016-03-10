b4c7dde2fbb7df2f1bc4c33a833790995abccb01
mEss1anh <dima-koval102496@rambler.ru>
2016-03-11 00:01:21 +0300

    ClickOpenCommand

--- PhotoEditor/ViewModel/MainViewModel.cs
+++ PhotoEditor/ViewModel/MainViewModel.cs
@@ -12,73 +12,28 @@ using System.Windows.Input;
 
 namespace PhotoEditor.ViewModel
 {
+
     class MainViewModel
     {
-        static ModelClassImage m = new ModelClassImage();
-        FileDialogClass fileDial = new FileDialogClass();
-        Command ClickOpenCommand = new Command(arg => fileDial.OpenFile());
-        
+        static ModelClassImage m;
+        FileDialogClass fileDial;
+        Command ClickOpenCommand;
         public MainViewModel()
         {
-            
+            m = new ModelClassImage();
+            fileDial = new FileDialogClass();
+            ClickOpenCommand = new Command(arg => fileDial.OpenFile());
         }
-
-        public class Command : ICommand
-        {
-            #region Constructor
-
-            public Command(Action<object> action)
-            {
-                ExecuteDelegate = action;
-            }
-
-            #endregion
-
-
-            #region Properties
-
-            public Predicate<object> CanExecuteDelegate { get; set; }
-            public Action<object> ExecuteDelegate { get; set; }
-
-            #endregion
-
-
-            #region ICommand Members
-
-            public bool CanExecute(object parameter)
-            {
-                if (CanExecuteDelegate != null)
-                {
-                    return CanExecuteDelegate(parameter);
-                }
-
-                return true;
-            }
-
-            public event EventHandler CanExecuteChanged
-            {
-                add { CommandManager.RequerySuggested += value; }
-                remove { CommandManager.RequerySuggested -= value; }
-            }
-
-            public void Execute(object parameter)
-            {
-                if (ExecuteDelegate != null)
-                {
-                    ExecuteDelegate(parameter);
-                }
-            }
-
-            #endregion
-
-            
-        }
-
 
         public class FileDialogClass
         {
             //public event PropertyChangedEventHandler PropertyChanged;
-            OpenFileDialog dialog = new OpenFileDialog();
+            OpenFileDialog dialog;
+            public FileDialogClass()
+            {
+                dialog = new OpenFileDialog();
+            }
+
 
             public void OpenFile()
             {
@@ -98,5 +53,58 @@ namespace PhotoEditor.ViewModel
                 }
             }
         }
+    }
+
+    
+
+    public class Command : ICommand
+    {
+        #region Constructor
+
+        public Command(Action<object> action)
+        {
+            ExecuteDelegate = action;
+        }
+
+        #endregion
+
+
+        #region Properties
+
+        public Predicate<object> CanExecuteDelegate { get; set; }
+        public Action<object> ExecuteDelegate { get; set; }
+
+        #endregion
+
+
+        #region ICommand Members
+
+        public bool CanExecute(object parameter)
+        {
+            if (CanExecuteDelegate != null)
+            {
+                return CanExecuteDelegate(parameter);
+            }
+
+            return true;
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public void Execute(object parameter)
+        {
+            if (ExecuteDelegate != null)
+            {
+                ExecuteDelegate(parameter);
+            }
+        }
+
+        #endregion
+
+
     }
 }
